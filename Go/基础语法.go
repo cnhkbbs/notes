6ce86c11df022c260d0041542d44738b469da141
bui~bui~bui~bui <53457442+cnhkbbs@users.noreply.github.com>
2023-10-02 17:48:26 +0800

    Update 基础语法.go

--- Go/基础语法.go
+++ Go/基础语法.go
@@ -1,11 +1,4 @@
-/*
- * @Author: cnhkbbs cnhkemail@foxmail.com
- * @Date: 2023-10-02 17:02:27
- * @LastEditors: cnhkbbs cnhkemail@foxmail.com
- * @LastEditTime: 2023-10-02 17:05:31
- * @FilePath: \demo1e:\notes\Go\基础语法.go
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
+
 package main
 
 import (
